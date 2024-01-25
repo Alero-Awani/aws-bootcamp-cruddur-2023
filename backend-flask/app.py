52408e4c693103a9562096d97c8ef4e541ccb059
Alero Awani <aleroawani18@gmail.com>
2024-01-25 15:59:34 +0100

    dockerfiles added

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -34,11 +34,6 @@ provider = TracerProvider()
 processor = BatchSpanProcessor(OTLPSpanExporter())
 provider.add_span_processor(processor)
 
-# X-RAY ----------
-xray_url = os.getenv("AWS_XRAY_URL")
-xray_recorder.configure(service="backend-flask", dynamic_naming=xray_url)
-XRayMiddleware(app, xray_recorder)
-
 # Show this in the logs within the backend-flask app STDOUT
 simple_processor = SimpleSpanProcessor(ConsoleSpanExporter())
 provider.add_span_processor(simple_processor)
@@ -56,6 +51,10 @@ app = Flask(__name__)
 FlaskInstrumentor().instrument_app(app)
 RequestsInstrumentor().instrument()
 
+# X-RAY ----------
+xray_url = os.getenv("AWS_XRAY_URL")
+xray_recorder.configure(service="backend-flask", dynamic_naming=xray_url)
+XRayMiddleware(app, xray_recorder)
 
 frontend = os.getenv("FRONTEND_URL")
 backend = os.getenv("BACKEND_URL")
